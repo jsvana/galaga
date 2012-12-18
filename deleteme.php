edf1c06bb028454d09ed502791c56fb3ed849fd5
Jay Vana <jay@stryder.io>
2012-12-18 17:18:49 -0500

    Update deleteme.php

--- deleteme.php
+++ deleteme.php
@@ -1,13 +1,13 @@
 while ($row = mysql_fetch_array($database)) {
-  echo "<tr>";
-	echo "<td>".$row['first_name']." ".$row['last_name']."</td>";
+    echo "<tr>";
+    echo "<td>".$row['first_name']." ".$row['last_name']."</td>";
 
-	for($i = 0; $i < 14; $i++) {
-		echo "<td><input type='checkbox' name='week1_".$row['id']."'";
-		if(($row['attendance'] >> $i) & 1){ echo " checked=\"checked\"";} 
-		if(($team_row['attendance'] >> $i) & 1 == '1'){ echo" disabled";};
-		echo " onclick='processAttendance(\"".$row['username']."\",".$i.",this.checked)'></td>";
-	}
+    for($i = 0; $i < 14; $i++) {
+        echo "<td><input type='checkbox' name='week1_".$row['id']."'";
+        if(($row['attendance'] >> $i) & 1){ echo " checked=\"checked\"";} 
+        if(($team_row['attendance'] >> $i) & 1 == '1'){ echo" disabled";};
+        echo " onclick='processAttendance(\"".$row['username']."\",".$i.",this.checked)'></td>";
+    }
 
-	echo "</tr>";
+    echo "</tr>";
 }
